47878c5ad1c01bdf4221da8c18e69c5e9d51b463
fchis <fchis@407f4433-bdb1-4852-b650-b45d22acb112>
2013-03-12 10:31:02 +0000

    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.4if-gl@12 407f4433-bdb1-4852-b650-b45d22acb112

--- src/main.c
+++ src/main.c
@@ -1,1 +1,14 @@
- 
+#include "dtd/dtd.tab.h"
+#include "xml/yy.tab.h" 
+
+int main(int argc, char **argv)
+{
+  int err;
+
+  yydebug = 1; // pour désactiver l'affichage de l'exécution du parser LALR, commenter cette ligne
+
+  err = yyparse();
+  if (err != 0) printf("Parse ended with %d error(s)\n", err);
+        else  printf("Parse ended with success\n", err);
+  return 0;
+}
