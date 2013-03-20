72b51d12efd6051115ba779eb88ce756d7cf1709
rgicquel <rgicquel@407f4433-bdb1-4852-b650-b45d22acb112>
2013-03-20 13:00:16 +0000

    Tabulations dans main.c
    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.4if-gl@57 407f4433-bdb1-4852-b650-b45d22acb112

--- src/main.c
+++ src/main.c
@@ -28,75 +28,75 @@ extern FILE * dtdin;
 
 int main(int argc, char **argv)
 {
-  Document * docXML = new Document();
-  //DocumentDTD * docDTD = new DocumentDTD();
+	Document * docXML = new Document();
+	//DocumentDTD * docDTD = new DocumentDTD();
 
-  parser(docXML, /*docDTD*/ NULL);
+	parser(docXML, /*docDTD*/ NULL);
 
-  verifXml(NULL, docXML);
+	verifXml(NULL, docXML);
 
-  return 0;
+	return 0;
 }
 
 /* ---VERIF XSLT------------------------------------- */
 bool verifXslt(Document xslt, DocumentDTD dtdXsl)
 {
 
-  return true;
+	return true;
 }
 
 
 /* ---PARSEUR------------------------------------- */
 bool parser(Document * docXML, DocumentDTD * docDTD)
 {
-  xmldebug = 1; // pour désactiver l'affichage de l'exécution du parser LALR, commenter cette ligne
+	xmldebug = 1; // pour désactiver l'affichage de l'exécution du parser LALR, commenter cette ligne
 
-  int err;
-  err = xmlparse(docXML);
-  if (err != 0)
-  {
-    printf("Parse ended with %d error(s)\n", err);
-  }
-  else
-  {
-    printf("Parse ended with success\n", err);
-  }
+	int err;
+	err = xmlparse(docXML);
+	if (err != 0)
+	{
+		printf("Parse ended with %d error(s)\n", err);
+	}
+	else
+	{
+		printf("Parse ended with success\n", err);
+	}
 
-  docXML->afficher();
+	docXML->afficher();
 
-  string nomDtd = docXML->getNomDtd();
-  if (nomDtd == "")
-  {
-    printf("Aucune dtd associée\n", err);
-  }
-  else
-  {
-    dtddebug = 1;
-    FILE *file;
-    file = fopen(nomDtd.c_str(), "r");
-  
-    if (!file)
-    {
-      fprintf(stderr, "Could not open %s \n", nomDtd.c_str());
-      return false;
-    }
-    
-    dtdin = file;
+	string nomDtd = docXML->getNomDtd();
+	if (nomDtd == "")
+	{
+		printf("Aucune dtd associée\n", err);
+	}
+	else
+	{
+		dtddebug = 1;
+		FILE *file;
+		file = fopen(nomDtd.c_str(), "r");
+	
+		if (!file)
+		{
+			fprintf(stderr, "Could not open %s \n", nomDtd.c_str());
+			return false;
+		}
+		
+		dtdin = file;
 
-    err = dtdparse();
-    if (err != 0)
-    {
-      printf("Parse ended with %d error(s)\n", err);
-    }
-    else
-    {
-      printf("Parse ended with success\n", err);
-    }
-    
-    fclose(file);
-  }
+		err = dtdparse();
+		if (err != 0)
+		{
+			printf("Parse ended with %d error(s)\n", err);
+		}
+		else
+		{
+			printf("Parse ended with success\n", err);
+		}
+		
+		fclose(file);
+	}
 
-  return true;
+	return true;
 }
 
 
@@ -105,10 +105,10 @@ bool parser(Document * docXML, DocumentDTD * docDTD)
 
 bool verifXml(DocumentDTD * dtd, Document * xml)
 {
-  if (dtd == NULL || xml == NULL)
-  {
-    return false;
-  }
+	if (dtd == NULL || xml == NULL)
+	{
+		return false;
+	}
 
 	ElementBalise * noeud;
 	map<string, ElementDTD*> * elts;
@@ -122,10 +122,10 @@ bool verifXml(DocumentDTD * dtd, Document * xml)
 bool verifNoeud(AbstractElement * abstractNoeud, map<string, ElementDTD*> * elts)
 {
 
-  if (abstractNoeud == NULL || elts == NULL)
-  {
-    return false;
-  }
+	if (abstractNoeud == NULL || elts == NULL)
+	{
+		return false;
+	}
 
 	ElementBalise * noeud = dynamic_cast<ElementBalise*>(abstractNoeud);
 
@@ -146,41 +146,41 @@ bool verifNoeud(AbstractElement * abstractNoeud, map<string, ElementDTD*> * elts
 	}
 
 	// vérification des attributs du noeud courant
-  set<AttributXML*> * attsXML = noeud->getSetAttribut();
-  set<AttributDTD*> * attsDTD = noeudDTD->getSetAttributDTD();
+	set<AttributXML*> * attsXML = noeud->getSetAttribut();
+	set<AttributDTD*> * attsDTD = noeudDTD->getSetAttributDTD();
 
-  set<AttributXML*>::iterator it;
-  set<AttributDTD*>::iterator it2;
-  bool trouve;
-  for (it = attsXML->begin(); it != attsXML->end(); it++)
-  {
-    trouve = false;
+	set<AttributXML*>::iterator it;
+	set<AttributDTD*>::iterator it2;
+	bool trouve;
+	for (it = attsXML->begin(); it != attsXML->end(); it++)
+	{
+		trouve = false;
 
-    for (it2 = attsDTD->begin(); it2 != attsDTD->end(); it2++)
-    {
-      if ((*it)->getNom() == (*it2)->getNom())
-      {
-        trouve = true;
-        break;
-      }
-    }
+		for (it2 = attsDTD->begin(); it2 != attsDTD->end(); it2++)
+		{
+			if ((*it)->getNom() == (*it2)->getNom())
+			{
+				trouve = true;
+				break;
+			}
+		}
 
-    if (!trouve)
-    {
-      return false;
-    }
-  }
+		if (!trouve)
+		{
+			return false;
+		}
+	}
 
 	// vérification des sous-éléments*/
-  list<AbstractElement*> * lstEltsXML = noeud->getLstAbstractElement();
-  list<AbstractElement*>::iterator it3;
-  for (it3 = lstEltsXML->begin(); it3 != lstEltsXML->end(); it3++)
-  {
-    if (!verifNoeud(*(it3), elts))
-    {
-      return false;
-    }
-  }
+	list<AbstractElement*> * lstEltsXML = noeud->getLstAbstractElement();
+	list<AbstractElement*>::iterator it3;
+	for (it3 = lstEltsXML->begin(); it3 != lstEltsXML->end(); it3++)
+	{
+		if (!verifNoeud(*(it3), elts))
+		{
+			return false;
+		}
+	}
 
-  return true;
+	return true;
 }
