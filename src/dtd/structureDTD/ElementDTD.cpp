bfd52dbb0dfce0afa3db94f6a714b8b6f1f11fc5
amerydemon <amerydemon@407f4433-bdb1-4852-b650-b45d22acb112>
2013-03-23 09:27:41 +0000

    corrigé regex dtd
    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.4if-gl@100 407f4433-bdb1-4852-b650-b45d22acb112

--- src/dtd/structureDTD/ElementDTD.cpp
+++ src/dtd/structureDTD/ElementDTD.cpp
@@ -76,9 +76,9 @@ void ElementDTD::Afficher() const
 
 string ElementDTD::getRegEx()
 {
-	string regEx="/";
+	string regEx="/^";
 	regEx += groupeSubElement->getRegEx();
-	regEx += "/";
+	regEx += "$/";
 }
 
 GroupeSubElement * ElementDTD::getGroupeSubElement()
