ed4281e5d72f2dfe782d1fbcf1d2fc0851a9a6ba
rgicquel <rgicquel@407f4433-bdb1-4852-b650-b45d22acb112>
2013-03-24 20:26:12 +0000

    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.4if-gl@136 407f4433-bdb1-4852-b650-b45d22acb112

--- src/tests/test.sh
+++ src/tests/test.sh
@@ -3,14 +3,14 @@
 lot1OK="01TestsXML/01Ok.xml"
 lot1KO="01TestsXML/02ErreurBalise.xml 01TestsXML/03ErreurNomBalise.xml"
 
-lot2OK=""
-lot2KO=""
+lot2OK="02TestsDTD/01Ok.dtd 02Ok(desordreOk).dtd 02TestsDTD/05OkDesordreAttributs.dtd"
+lot2KO="02TestsDTD/03ElementAuteurManquant.dtd 02TestsDTD/04ErreurBalise.dtd 02TestsDTD/06ErreurAttributManquant.dtd"
 
-lot3OK=""
-lot3KO=""
+lot3OK="03TestsConformes/01Ok.xml 03TestsConformes/04OkOrdreDif.xml"
+lot3KO="03TestsConformes/02Ok.xml 03TestsConformes/03Ok.xml 03TestsConformes/05XmlNonConforme.xml"
 
-lot4OK=""
-lot4KO=""
+lot4OK="04TestsXSL/01Ok.xsl"
+lot4KO="04TestsXSL/erreurBalise2.xsl 04TestsXSL/noStylesheet.xsl 04TestsXSL/ordreStylesheet.xsl"
 
 erreur=0
 
