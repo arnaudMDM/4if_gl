b73850337ce64d70fdd18296dbbfe8da07552c24
rgicquel <rgicquel@407f4433-bdb1-4852-b650-b45d22acb112>
2013-03-24 20:18:39 +0000

    test.sh
    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.4if-gl@135 407f4433-bdb1-4852-b650-b45d22acb112

--- src/tests/test.sh
+++ src/tests/test.sh
@@ -3,7 +3,7 @@
 lot1OK="01TestsXML/01Ok.xml"
 lot1KO="01TestsXML/02ErreurBalise.xml 01TestsXML/03ErreurNomBalise.xml"
 
-lot2OK="02TestsDTD"
+lot2OK=""
 lot2KO=""
 
 lot3OK=""
@@ -15,15 +15,117 @@ lot4KO=""
 erreur=0
 
 option="-cxml"
+resultat=0
 
-for fichier in lot1OK
+for fichier in $lot1OK
 do
-	../analyse $option $fichier
-	if [$? -ne 0]
+	../analyse $option $fichier > /dev/null 2> /dev/null
+	if [ $? -ne $resultat ]
 	then
-		echo "Résultat inattendu pour $fichier"
+		echo "$fichier : résultat inattendu"
 		erreur=1
+	else
+		echo "$fichier : OK"
 	fi
 done
 
+resultat=1
 
+for fichier in $lot1KO
+do
+	../analyse $option $fichier > /dev/null 2> /dev/null
+	if [ $? -ne $resultat ]
+	then
+		echo "$fichier : résultat inattendu"
+		erreur=1
+	else
+		echo "$fichier : OK"
+	fi
+done
+
+option="-cdtd"
+resultat=0
+
+for fichier in $lot2OK
+do
+	../analyse $option $fichier > /dev/null 2> /dev/null
+	if [ $? -ne $resultat ]
+	then
+		echo "$fichier : résultat inattendu"
+		erreur=1
+	else
+		echo "$fichier : OK"
+	fi
+done
+
+resultat=1
+
+for fichier in $lot2KO
+do
+	../analyse $option $fichier > /dev/null 2> /dev/null
+	if [ $? -ne $resultat ]
+	then
+		echo "$fichier : résultat inattendu"
+		erreur=1
+	else
+		echo "$fichier : OK"
+	fi
+done
+
+option="-check"
+resultat=0
+
+for fichier in $lot3OK
+do
+	../analyse $option $fichier > /dev/null 2> /dev/null
+	if [ $? -ne $resultat ]
+	then
+		echo "$fichier : résultat inattendu"
+		erreur=1
+	else
+		echo "$fichier : OK"
+	fi
+done
+
+resultat=1
+
+for fichier in $lot3KO
+do
+	../analyse $option $fichier > /dev/null 2> /dev/null
+	if [ $? -ne $resultat ]
+	then
+		echo "$fichier : résultat inattendu"
+		erreur=1
+	else
+		echo "$fichier : OK"
+	fi
+done
+
+option="-cxsl"
+resultat=0
+
+for fichier in $lot4OK
+do
+	../analyse $option $fichier > /dev/null 2> /dev/null
+	if [ $? -ne $resultat ]
+	then
+		echo "$fichier : résultat inattendu"
+		erreur=1
+	else
+		echo "$fichier : OK"
+	fi
+done
+
+resultat=1
+
+for fichier in $lot4KO
+do
+	../analyse $option $fichier > /dev/null 2> /dev/null
+	if [ $? -ne $resultat ]
+	then
+		echo "$fichier : résultat inattendu"
+		erreur=1
+	else
+		echo "$fichier : OK"
+	fi
+done
