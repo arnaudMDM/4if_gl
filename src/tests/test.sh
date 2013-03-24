bdaa8b90a5e99e3dfa6b3ba99ec5c6fb84cb63f7
rgicquel <rgicquel@407f4433-bdb1-4852-b650-b45d22acb112>
2013-03-24 20:44:02 +0000

    tests automatisés
    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.4if-gl@137 407f4433-bdb1-4852-b650-b45d22acb112

--- src/tests/test.sh
+++ src/tests/test.sh
@@ -15,117 +15,141 @@ lot4KO="04TestsXSL/erreurBalise2.xsl 04TestsXSL/noStylesheet.xsl 04TestsXSL/ordr
 erreur=0
 
 option="-cxml"
-resultat=0
+testo="-ne"
 
 for fichier in $lot1OK
 do
+	echo "../analyse $option $fichier"
 	../analyse $option $fichier > /dev/null 2> /dev/null
-	if [ $? -ne $resultat ]
+	if [ $? $testo 0 ]
 	then
-		echo "$fichier : rÈsultat inattendu"
+		echo "$fichier : r√©sultat inattendu"
 		erreur=1
 	else
 		echo "$fichier : OK"
 	fi
+	echo
 done
 
-resultat=1
+testo="-eq"
 
 for fichier in $lot1KO
 do
+	echo "../analyse $option $fichier"
 	../analyse $option $fichier > /dev/null 2> /dev/null
-	if [ $? -ne $resultat ]
+	if [ $? $testo 0 ]
 	then
-		echo "$fichier : rÈsultat inattendu"
+		echo "$fichier : r√©sultat inattendu"
 		erreur=1
 	else
 		echo "$fichier : OK"
 	fi
+	echo
 done
 
 option="-cdtd"
-resultat=0
+testo="-ne"
 
 for fichier in $lot2OK
 do
+	echo "../analyse $option $fichier"
 	../analyse $option $fichier > /dev/null 2> /dev/null
-	if [ $? -ne $resultat ]
+	if [ $? $testo 0 ]
 	then
-		echo "$fichier : rÈsultat inattendu"
+		echo "$fichier : r√©sultat inattendu"
 		erreur=1
 	else
 		echo "$fichier : OK"
 	fi
+	echo
 done
 
-resultat=1
+testo="-eq"
 
 for fichier in $lot2KO
 do
+	echo "../analyse $option $fichier"
 	../analyse $option $fichier > /dev/null 2> /dev/null
-	if [ $? -ne $resultat ]
+	if [ $? $testo 0 ]
 	then
-		echo "$fichier : rÈsultat inattendu"
+		echo "$fichier : r√©sultat inattendu"
 		erreur=1
 	else
 		echo "$fichier : OK"
 	fi
+	echo
 done
 
 option="-check"
-resultat=0
+testo="-ne"
 
 for fichier in $lot3OK
 do
+	echo "../analyse $option $fichier"
 	../analyse $option $fichier > /dev/null 2> /dev/null
-	if [ $? -ne $resultat ]
+	if [ $? $testo 0 ]
 	then
-		echo "$fichier : rÈsultat inattendu"
+		echo "$fichier : r√©sultat inattendu"
 		erreur=1
 	else
 		echo "$fichier : OK"
 	fi
+	echo
 done
 
-resultat=1
+testo="-eq"
 
 for fichier in $lot3KO
 do
+	echo "../analyse $option $fichier"
 	../analyse $option $fichier > /dev/null 2> /dev/null
-	if [ $? -ne $resultat ]
+	if [ $? $testo 0 ]
 	then
-		echo "$fichier : rÈsultat inattendu"
+		echo "$fichier : r√©sultat inattendu"
 		erreur=1
 	else
 		echo "$fichier : OK"
 	fi
+	echo
 done
 
 option="-cxsl"
-resultat=0
+testo="-ne"
 
 for fichier in $lot4OK
 do
+	echo "../analyse $option $fichier"
 	../analyse $option $fichier > /dev/null 2> /dev/null
-	if [ $? -ne $resultat ]
+	if [ $? $testo 0 ]
 	then
-		echo "$fichier : rÈsultat inattendu"
+		echo "$fichier : r√©sultat inattendu"
 		erreur=1
 	else
 		echo "$fichier : OK"
 	fi
+	echo
 done
 
-resultat=1
+testo="-eq"
 
 for fichier in $lot4KO
 do
+	echo "../analyse $option $fichier"
 	../analyse $option $fichier > /dev/null 2> /dev/null
-	if [ $? -ne $resultat ]
+	if [ $? $testo 0 ]
 	then
-		echo "$fichier : rÈsultat inattendu"
+		echo "$fichier : r√©sultat inattendu"
 		erreur=1
 	else
 		echo "$fichier : OK"
 	fi
+	echo
 done
+
+echo
+if [ $erreur -eq 0 ]
+then
+	echo "Lot de tests r√©alis√© sans erreur"
+else
+	echo "Une ou plusieurs erreurs sont survenues dans le lot de tests"
+fi
